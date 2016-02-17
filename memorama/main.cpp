5af7929a0f25667ab12b4c9cde1ef5fcc1336e4f
Jorge Luis Perales <jorgelp94@MacBook-Pro-de-Jorge.local>
2016-02-17 13:03:28 -0600

    examen

--- memorama/main.cpp
+++ memorama/main.cpp
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -18,12 +20,15 @@ float delta=0.1;
 int turnos = 0;
 int clickCarta = 0;
 
+int anchoGlobal = 700, altoGlobal = 640;
+
 int sumaTotal = 0;
-bool start = false;
+bool start = false, help = true;
 
 int mazo[16] = {5,4,6,7,1,2,3,0,4,3,0,7,1,5,6,2};
 bool expuesta[16] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
 bool destapado[16] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+bool hoverCards[16] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
 
 bool allTrue() {
     bool status = true;
@@ -47,21 +52,39 @@ void myTimer(int i) {
     glutTimerFunc(100,myTimer,1);
 }
 
+void revuelveBaraja() {
+    
+    for (int i=0; i<16; i++){
+        mazo[i] = -1;
+    }
+    
+    int x;
+    srand(time(NULL));
+    
+    for (int i=0; i<=7; i++){
+        for (int j=0; j<2; j++) {
+            x = rand() % 16;
+            while (mazo[x] != -1){
+                x = (x + 1 ) % 16;
+            }
+            mazo[x] = i;
+            
+        }
+    }
+}
 
-
-int	screenWidth = 700, screenHeight = 640;
 
 void reshape(int ancho, int alto) {
     
-    screenHeight = alto;
-    screenWidth = ancho;
+    altoGlobal = alto;
+    anchoGlobal = ancho;
     
-    glViewport(0.0,0.0,(GLdouble) ancho,(GLdouble)  alto);
+    glViewport(0.0,0.0,ancho,alto);
     glMatrixMode( GL_PROJECTION );
     glLoadIdentity();
     
     // hace la conversion a openGL
-    gluOrtho2D( 0.0,(GLdouble) ancho, 0.0,(GLdouble)  alto);
+    gluOrtho2D( 0.0, 700, 0.0, 640);
     
     
 }
@@ -70,7 +93,7 @@ void despliegaMazo(){
     
     float posX, posYbottom, posYtop;
     posX = 0;
-    posYtop = 640;
+    posYtop = altoGlobal;
     posYbottom = 400;
     
     for (int i=0; i<=15; i++){
@@ -83,6 +106,9 @@ void despliegaMazo(){
                 glColor3ub(255, 255, 0);
             } else {
                glColor3ub(255, 255, 255);
+            }
+            if (hoverCards[i] && !expuesta[i]) {
+                glColor3f(255, 0, 0);
             }
             
             glVertex2f(posX, posYbottom);
@@ -99,6 +125,9 @@ void despliegaMazo(){
                 glColor3ub(255, 255, 0);
             } else {
                 glColor3ub(100, 100, 100);
+            }
+            if (hoverCards[i] && !expuesta[i]) {
+                glColor3f(255, 0, 0);
             }
             
             glVertex2f(posX, posYbottom);
@@ -127,6 +156,31 @@ void despliegaMazo(){
         
         posX += 43.75;
     }
+}
+
+void muestraAyuda() {
+    float posX, posYbottom, posYtop;
+    posX = 0;
+    posYtop = 640;
+    posYbottom = 400;
+    if (help) {
+        for (int i=0; i<=15; i++){
+            
+            GLint k;
+            char mensaje7 [200] = "";
+            sprintf(mensaje7,"%d", mazo[i]);
+            glColor3f(0, 0, 0);
+            glRasterPos2f(posX+5, posYbottom+200); // inicializa raster position
+            for (k=0; mensaje7[k] != '\0'; k++) {
+                glColor3f(0, 0, 1);
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, mensaje7[k]);
+            }
+            
+            posX += 43.75;
+        }
+    }
+    
+
 }
 
 void dibujaInfo() {
@@ -163,41 +217,55 @@ void dibujaInfo() {
             glColor3f(1, 1, 1);
             glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje3[k]);
         }
+        if (help) {
+            
+            muestraAyuda();
+            
+            char mensaje4 [200] = "";
+            sprintf(mensaje4, "%s", "Autores: Jorge Perales, Matricula:00813101");
+            glColor3f(1, 1, 1);
+            glRasterPos2f(20, 130); // inicializa raster position
+            for (k=0; mensaje4[k] != '\0'; k++) {
+                glColor3f(1, 1, 1);
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje4[k]);
+            }
+            char mensaje5 [200] = "";
+            sprintf(mensaje5, "%s", "Diego Aleman, Matricula:1139700");
+            glColor3f(1, 1, 1);
+            glRasterPos2f(100, 100); // inicializa raster position
+            for (k=0; mensaje5[k] != '\0'; k++) {
+                glColor3f(1, 1, 1);
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje5[k]);
+            }
+            
+            char mensaje [200] = "";
+            sprintf(mensaje, "%s", "I-Iniciar, P-Pausa, R-Reset");
+            glColor3f(1, 1, 1);
+            glRasterPos2f(100, 70); // inicializa raster position
+            for (k=0; mensaje[k] != '\0'; k++) {
+                glColor3f(1, 1, 1);
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje[k]);
+            }
+            
+            char mensaje2 [200] = "";
+            sprintf(mensaje2, "%s", "Esc-Salir");
+            glColor3f(1, 1, 1);
+            glRasterPos2f(400, 70);
+            for (k=0; mensaje2[k] != '\0'; k++) {
+                glColor3f(1, 1, 1);
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje2[k]);
+            }
+            
+            char mensaje6 [200] = "";
+            sprintf(mensaje6, "%s", "A-Ayuda");
+            glColor3f(1, 1, 1);
+            glRasterPos2f(100, 30);
+            for (k=0; mensaje6[k] != '\0'; k++) {
+                glColor3f(1, 1, 1);
+                glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje6[k]);
+            }
+        }
         
-        char mensaje4 [200] = "";
-        sprintf(mensaje4, "%s", "Autores: Jorge Perales, Matricula:00813101");
-        glColor3f(1, 1, 1);
-        glRasterPos2f(20, 130); // inicializa raster position
-        for (k=0; mensaje4[k] != '\0'; k++) {
-            glColor3f(1, 1, 1);
-            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje4[k]);
-        }
-        char mensaje5 [200] = "";
-        sprintf(mensaje5, "%s", "Diego Aleman, Matricula:1139700");
-        glColor3f(1, 1, 1);
-        glRasterPos2f(100, 100); // inicializa raster position
-        for (k=0; mensaje5[k] != '\0'; k++) {
-            glColor3f(1, 1, 1);
-            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje5[k]);
-        }
-        
-        char mensaje [200] = "";
-        sprintf(mensaje, "%s", "I-Iniciar, P-Pausa, R-Reset");
-        glColor3f(1, 1, 1);
-        glRasterPos2f(100, 70); // inicializa raster position
-        for (k=0; mensaje[k] != '\0'; k++) {
-            glColor3f(1, 1, 1);
-            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje[k]);
-        }
-        
-        char mensaje2 [200] = "";
-        sprintf(mensaje2, "%s", "Esc-Salir");
-        glColor3f(1, 1, 1);
-        glRasterPos2f(400, 70);
-        for (k=0; mensaje2[k] != '\0'; k++) {
-            glColor3f(1, 1, 1);
-            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, mensaje2[k]);
-        }
     }
 }
 
@@ -246,8 +314,9 @@ void myDisplay()
     glClear( GL_COLOR_BUFFER_BIT );
     glColor3f( 1.0f, 0.0f, 0.0f ); //Color para pintar
     cronometro();
-    dibujaInfo();
+    //dibujaInfo();
     despliegaMazo();
+    dibujaInfo();
     
     glutSwapBuffers();
     
@@ -264,6 +333,12 @@ void myKeyboard(unsigned char theKey, int mouseX, int mouseY) {
             
         case 'R':
         case 'r':
+            revuelveBaraja();
+            for (int i = 0; i < 16; i++) {
+                
+                hoverCards[i] = false;
+                
+            }
             start = false;
             turnos = 0;
             clickCarta = 0;
@@ -278,8 +353,22 @@ void myKeyboard(unsigned char theKey, int mouseX, int mouseY) {
             start = false;
             break;
             
+        case 'A':
+        case 'a':
+            if (start) {
+                if (help) {
+                    help = false;
+                } else {
+                    help = true;
+                }
+            }
+            
+            break;
+            
         case 27:
             exit(-1);
+            
+            
             
         default:
             break;		      // do nothing
@@ -287,9 +376,9 @@ void myKeyboard(unsigned char theKey, int mouseX, int mouseY) {
 }
 
 int calculaCarta(int x, int y){
-    if (y >= 400 && y <= 640) {
+    if (y >= 400 && y <= altoGlobal) {
         
-        float carta = x/43.75;
+        float carta = x/(anchoGlobal/16.0);
         return (int)carta;
     } else {
         return -1;
@@ -352,17 +441,31 @@ void mouse(int button, int state, int x, int y){
     }
 }
 
+void changeOnHover(int x, int y) {
+    if (start) {
+        int carta = calculaCarta(x, altoGlobal - y);
+        hoverCards[carta] = true;
+        for (int i = 0; i < 16; i++) {
+            if (i != carta) {
+                hoverCards[i] = false;
+            }
+        }
+        glutPostRedisplay();
+    }
+    
+}
 
 int main( int argc, char ** argv ) {
     
     glutInit( &argc, argv );
     glutInitDisplayMode( GLUT_DOUBLE| GLUT_RGB); // dos buffers
-    glutInitWindowSize( screenWidth, screenHeight );
+    glutInitWindowSize( 700, 640 );
     glutInitWindowPosition( 0, 0 );// create window
     glutCreateWindow( "Memorama" );
     glutDisplayFunc( myDisplay );
     glutKeyboardFunc(myKeyboard);
     glutMouseFunc(mouse);
+    glutPassiveMotionFunc(changeOnHover);
     glutReshapeFunc(reshape);
     glutTimerFunc(100,myTimer,1);
     
